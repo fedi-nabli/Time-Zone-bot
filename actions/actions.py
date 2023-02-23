a772b9ece0d618f81c7960072564128610e819ab
Fedi Nabli <fedinabli@gmail.com>
2023-02-23 19:26:24 +0100

    Added chatbot actions

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,38 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+timezones = {
+  "London": "UTC+1:00",
+  "Sofia": "UTC+3:00",
+  "Sousse": "UTC+1:00",
+  "Tunis": "UTC+1:00",
+  "Lisbon": "UTC+1:00",
+  "Mumbai": "UTC+5:30"
+}
+
+class ActionShowTimeZone(Action):
+    
+  def name(self) -> Text:
+    return "action_show_time_zone"
+  
+  def run(self, dispatcher: CollectingDispatcher,
+          tracker: Tracker,
+          domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    
+    city = tracker.get_slot("city")
+    timzeone = timezones.get(str(city))
+
+    if timzeone is None:
+      output = "Could not find the time zone of {}".format(city)
+    else:
+      output = "Time zone of {} is {}".format(city, timzeone)
+
+    dispatcher.utter_message(text=output)
+    
+    return []
